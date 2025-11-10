d50847984e0139dddd37ad954090752ef210f08b
filipebpitombeira <116564420+filipebpitombeira@users.noreply.github.com>
2025-11-10 22:58:19 +0000

    commit Vesão Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,11 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+    int população;
+    float area;
+    
+
+
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
